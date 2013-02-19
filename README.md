6e0439302df6c5c20e6a574efc490f4bca57e21d
John Ericksen <johncarl81@gmail.com>
2013-02-19 16:29:07 -0700

    added LICENSE and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arduino LOL Automata
+
+Displays scrolling Automata on the Arduino micro-controller equiped with a Lots of Lights (LOL) Shield.
--- automata.ino
+++ automata.ino
@@ -1,3 +1,21 @@
+/*
+    LOL Automata for the Arduino Micro-Controller
+    Copyright (C) 2013 John Ericksen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
 #include "Charliplexing.h"
 
 #define SCREENX 14
@@ -5,22 +23,17 @@
 #define RULESIZE 8
 #define MAX 1000
 
-int rule = 110;
+int rule = 30;
 int row = 0;
 int offset = 0;
 
 byte buffer[SCREENX][SCREENY];
 byte rules[8];
 
-
-// This function is called once, when the sketch starts.
 void setup() {
   LedSign::Init(GRAYSCALE);
   randomSeed(analogRead(0));
-  
   reset();
-  
-  row++;
 }
 
 
@@ -28,6 +41,7 @@ void setup() {
 // This function is called every frame.
 void loop() {
   
+  //update the last row, based on rule interpretation of the previous row
   for(int i = 1; i < SCREENX; i++){
     byte sample = 0;
     bitWrite(sample, 2, buffer[(i-1 + SCREENX) % SCREENX][row-1]);
@@ -37,15 +51,18 @@ void loop() {
     buffer[i][row] = rules[sample];
   }
   
+  //update the entire LedSign
   for(int i = 0; i < SCREENX; i++){
     for(int j = 0; j < SCREENY; j++){
       LedSign::Set( i, j, buffer[i][j]);
     }
   }
   if(row < SCREENY-1){
+    //move the row down (until it reaches the end of the screen)
     row++;
   }
   else{
+    //move the screen up
     for(int i = 0; i < SCREENX; i++){
       for(int j = 0; j < SCREENY; j++){
         buffer[i][j-1] = buffer[i][j];
@@ -53,6 +70,7 @@ void loop() {
     }
   }
   if(row > MAX){
+    //reset and choose a new rule if MAX is hit.
     rule = random(256);
     reset();
   }
@@ -62,19 +80,22 @@ void loop() {
 void reset(){
   row = 0;
   
+  //setup the rules based on wolfram notation
   for(int i = 0; i < 8; i++){
     rules[i] = bitRead(rule, i);
   }
   
+  //clear the buffer
   for(int i = 0; i < SCREENX; i++){
     for(int j = 1; j < SCREENY; j++){
       buffer[i][j] = 0;
     }
   }
   
+  //set the last row to a random sample
   for(int i = 0; i < SCREENX; i++){
     buffer[i][row] = random(2);
   }
-  //buffer[(SCREENX+MARGINS)/2][0] = 1;
+  row = 1;
 }
 
