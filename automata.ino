f121e09ee86659b104f7e6fa646e1e29adce31c9
John Ericksen <johncarl81@gmail.com>
2013-02-19 12:04:38 -0700

    looped buffer

--- automata.ino
+++ automata.ino
@@ -3,35 +3,23 @@
 #define SCREENX 14
 #define SCREENY 9
 #define RULESIZE 8
+#define MAX 1000
 
-int rule = 30;
+int rule = 110;
 int row = 0;
 int offset = 0;
 
-byte buffer[SCREENX+2][SCREENY];
+byte buffer[SCREENX][SCREENY];
 byte rules[8];
 
 
 // This function is called once, when the sketch starts.
 void setup() {
   LedSign::Init(GRAYSCALE);
-  Serial.begin(9600);
   randomSeed(analogRead(0));
   
-  byte b = 0;
+  reset();
   
-  bitWrite(b, 2, 1);
-  
-  for(int i = 0; i < 8; i++){
-    rules[i] = bitRead(rule, i);
-    Serial.println(rules[i]);
-  }
-  
-  Serial.println(b);
-  
-  for(int i = 0; i < SCREENX; i++){
-    buffer[i][row] = random(2);
-  }
   row++;
 }
 
@@ -40,32 +28,53 @@ void setup() {
 // This function is called every frame.
 void loop() {
   
-  for(int i = 0; i < SCREENX; i++){
+  for(int i = 1; i < SCREENX; i++){
     byte sample = 0;
-    bitWrite(sample, 2, buffer[i][row-1]);
-    bitWrite(sample, 1, buffer[i+1][row-1]);
-    bitWrite(sample, 0, buffer[i+2][row-1]);
-    Serial.print("Sample: ");
-    Serial.println(sample);
+    bitWrite(sample, 2, buffer[(i-1 + SCREENX) % SCREENX][row-1]);
+    bitWrite(sample, 1, buffer[i][row-1]);
+    bitWrite(sample, 0, buffer[(i+1) % SCREENX][row-1]);
     
-    buffer[i + 1][row] = rules[sample];
+    buffer[i][row] = rules[sample];
   }
   
   for(int i = 0; i < SCREENX; i++){
     for(int j = 0; j < SCREENY; j++){
-      LedSign::Set( i, j, buffer[i+1][j]);
+      LedSign::Set( i, j, buffer[i][j]);
     }
   }
   if(row < SCREENY-1){
     row++;
   }
   else{
-    for(int i = 0; i < SCREENX+2; i++){
+    for(int i = 0; i < SCREENX; i++){
       for(int j = 0; j < SCREENY; j++){
         buffer[i][j-1] = buffer[i][j];
       }
     }
   }
-  //delay(1000);
+  if(row > MAX){
+    rule = random(256);
+    reset();
+  }
+  delay(200);
+}
+
+void reset(){
+  row = 0;
+  
+  for(int i = 0; i < 8; i++){
+    rules[i] = bitRead(rule, i);
+  }
+  
+  for(int i = 0; i < SCREENX; i++){
+    for(int j = 1; j < SCREENY; j++){
+      buffer[i][j] = 0;
+    }
+  }
+  
+  for(int i = 0; i < SCREENX; i++){
+    buffer[i][row] = random(2);
+  }
+  //buffer[(SCREENX+MARGINS)/2][0] = 1;
 }
 
